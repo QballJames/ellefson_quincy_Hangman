790689bb47971359b4b2ce5529425788292b7cd9
Quincy Ellefson <w10025478@usm.edu>
2021-12-02 23:00:42 -0600

    Add project files.

---
+++ ellefson_quincy_Hangman/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace ellefson_quincy_Hangman
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblFeedback = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.letterGuess = new System.Windows.Forms.TextBox();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.secretWord = new System.Windows.Forms.Label();
+            this.hangmanPicture = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.hangmanPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblFeedback
+            // 
+            this.lblFeedback.AutoSize = true;
+            this.lblFeedback.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFeedback.Location = new System.Drawing.Point(12, 108);
+            this.lblFeedback.Name = "lblFeedback";
+            this.lblFeedback.Size = new System.Drawing.Size(164, 31);
+            this.lblFeedback.TabIndex = 0;
+            this.lblFeedback.Text = "Take a Guess!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(14, 164);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Word:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 255);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(176, 31);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Guess a Letter:";
+            // 
+            // letterGuess
+            // 
+            this.letterGuess.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.letterGuess.Location = new System.Drawing.Point(194, 252);
+            this.letterGuess.MaxLength = 1;
+            this.letterGuess.Name = "letterGuess";
+            this.letterGuess.Size = new System.Drawing.Size(100, 39);
+            this.letterGuess.TabIndex = 3;
+            this.letterGuess.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuess.Location = new System.Drawing.Point(18, 296);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(129, 53);
+            this.btnGuess.TabIndex = 6;
+            this.btnGuess.Text = "Guess";
+            this.btnGuess.UseVisualStyleBackColor = true;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // secretWord
+            // 
+            this.secretWord.AutoSize = true;
+            this.secretWord.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.secretWord.Location = new System.Drawing.Point(100, 164);
+            this.secretWord.Name = "secretWord";
+            this.secretWord.Size = new System.Drawing.Size(137, 31);
+            this.secretWord.TabIndex = 7;
+            this.secretWord.Text = "secretWord";
+            // 
+            // hangmanPicture
+            // 
+            this.hangmanPicture.Location = new System.Drawing.Point(379, 36);
+            this.hangmanPicture.Name = "hangmanPicture";
+            this.hangmanPicture.Size = new System.Drawing.Size(180, 274);
+            this.hangmanPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.hangmanPicture.TabIndex = 8;
+            this.hangmanPicture.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.hangmanPicture);
+            this.Controls.Add(this.secretWord);
+            this.Controls.Add(this.btnGuess);
+            this.Controls.Add(this.letterGuess);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblFeedback);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.hangmanPicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFeedback;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox letterGuess;
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.Label secretWord;
+        private System.Windows.Forms.PictureBox hangmanPicture;
+    }
+}
+
---
+++ ellefson_quincy_Hangman/Form1.cs
@@ -0,0 +1,134 @@
+﻿/*Program Author: Quincy Ellefson
+ * 
+ * Assignment: Bonus Opportunity
+ * 
+ * Description: I ran out of time but this was intended to be a hangman game.
+ *              I'll be honest it's not functionally sound but I think I was
+ *              going in the right direction. The letterGuess text box accepts
+ *              lowercase despite all the words being uppercase. I also messed up the
+ *              translation from the wordList to the wordLetters character array and 
+ *              now the words are all out of order. Oh and the pictureBox functionality
+ *              isn't working. I guess this is better than not submitting anything?
+ *              
+ */
+
+
+
+using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ellefson_quincy_Hangman
+{
+    public partial class Form1 : Form
+    {
+        Random rand = new Random();
+        string[] wordList =
+        {
+            "COMPUTER",
+            "MINECRAFT",
+            "FINALS",
+            "COFFEE",
+            "KEYBOARD",
+            "SPONGEBOB",
+            "LEBRON"
+        };
+        int guessAttempts;
+        char letterGuessed;
+        public Char[] wordLetters;
+        public bool guessCorrect;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loadWord();//Calls the loadWord function to populate the secretWord label
+            hangmanPicture.Image = Properties.Resources.hang0;
+            guessAttempts = 0;
+        }
+
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            letterGuessed = letterGuess.Text[0];
+
+            if (wordLetters.Contains(letterGuessed))
+            {
+                string sameLetter = Char.ToString(letterGuessed);
+                lblFeedback.Text = "Nice Guess!";
+                for (int i = 0; i < wordLetters.Length; i++)
+                {
+                if (wordLetters[i] == letterGuessed)
+                {
+                    guessCorrect = true;
+                        secretWord.Text = secretWord.Text.Remove(i, 1);
+                        secretWord.Text = secretWord.Text.Insert(1, sameLetter);
+                }
+                else
+                {
+                    guessCorrect = false;
+                        lblFeedback.Text = "That was incorrect!";
+                        guessAttempts++;
+
+                }
+                    checkGuess();
+            }
+        }
+    }
+
+        private void loadWord()
+        {
+            string randWord = wordList[rand.Next(0, wordList.Length)];
+            wordLetters = randWord.ToCharArray();
+
+            secretWord.Text = "";
+            for (int i = 0; i < wordLetters.Length; i++)
+            {
+                secretWord.Text = secretWord.Text.Insert(i, "*");
+            }
+        }
+
+        private void checkGuess()
+        {
+            if(guessAttempts is 1)
+            {
+                hangmanPicture.Image = Properties.Resources.hang1;
+            }
+            if (guessAttempts is 2)
+            {
+                hangmanPicture.Image = Properties.Resources.hang2;
+            }
+            if (guessAttempts is 3)
+            {
+                hangmanPicture.Image = Properties.Resources.hang3;
+            }
+            if (guessAttempts is 4)
+            {
+                hangmanPicture.Image = Properties.Resources.hang4;
+            }
+            if (guessAttempts is 5)
+            {
+                hangmanPicture.Image = Properties.Resources.hang5;
+            }
+            if (guessAttempts is 6)
+            {
+                hangmanPicture.Image = Properties.Resources.hang6;
+            }
+            if (guessAttempts is 7)
+            {
+                hangmanPicture.Image = Properties.Resources.hang7;
+            }
+        }
+    }
+}
